caa34c33c1182b82be7cfb0998b71cdc0776c701
ageronimo <angelageroniimo@gmail.com>
2017-03-13 08:28:19 -1000

    learning about js dom

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("sanity check");
+
+//Finding HTML Elements
+// console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
+
+console.log(document.getElementsByTagName("div"));
+var tagElement = document.getElementsByTagName("div");
+console.log(tagElement);
